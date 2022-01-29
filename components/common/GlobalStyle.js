ef937e0685a324fa3701fea4aac894580d2704c6
Pili <pilichanampe@hotmail.com>
2022-01-29 02:28:09 -0300

    theme config not working yet

--- components/common/GlobalStyle.js
+++ components/common/GlobalStyle.js
@@ -1,10 +1,16 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+/* css body */
 html,
 body {
   padding: 0;
   margin: 0;
   font-family: -apple-system, Montserrat, Segoe UI, Roboto, Oxygen,
     Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-  font-weight: 900;
+  font-weight: 600;
+  max-width: 1464px;
+  color: ${props => props.theme.disabled};
 }
 
 a {
@@ -15,3 +21,4 @@ a {
 * {
   box-sizing: border-box;
 }
+`;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from '../components/common/GlobalStyle';
+import { darkTheme, lightTheme } from '../components/common/Theme';
+darkTheme
+// import '../styles/globals.css';
+import { useState } from 'react';
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const {theme, setTheme} = useState('light');
+
+  return (
+    <ThemeProvider theme={ theme === "light" ? lightTheme : darkTheme }>
+      <GlobalStyle/>
+      <Component {...pageProps} />
+    </ThemeProvider>
+
+  )
 }
 
 export default MyApp
