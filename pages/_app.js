0e82302ddeff89f89eed90a155025aed0fe4667b
Pili <pilichanampe@hotmail.com>
2022-01-31 19:52:42 -0300

    config theme.js and GlobalStyle.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,13 @@
-import { ThemeProvider } from 'styled-components';
-import { GlobalStyle } from '../components/common/GlobalStyle';
-import { darkTheme, lightTheme } from '../components/common/Theme';
-darkTheme
-// import '../styles/globals.css';
-import { useState } from 'react';
-
+import { GlobalStyle } from '../styles/GlobalStyle';
+import { theme } from '../styles/theme';
+import { ThemeProvider } from "styled-components";
 
 function MyApp({ Component, pageProps }) {
-  const {theme, setTheme} = useState('light');
-
   return (
-    <ThemeProvider theme={ theme === "light" ? lightTheme : darkTheme }>
-      <GlobalStyle/>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <Component {...pageProps} />
     </ThemeProvider>
-
-  )
+  ) 
 }
-
-export default MyApp
+export default MyApp;
---
+++ styles/GlobalStyle.js
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    padding: 0;
+    margin: 80px;
+    font-family: -apple-system, Montserrat, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    font-weight: 600;
+    max-width: 1464px;
+    color: ${props => props.theme.neutrals.n300};
+    display: flex;
+    flex-direction: column;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`;
+
+export const Row = createGlobalStyle`
+`
