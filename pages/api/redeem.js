64e4f14adeddb0156c04054d208d702466569127
Pili <pilichanampe@hotmail.com>
2022-02-04 00:34:29 -0300

    data fetch in redeem and history

--- pages/api/redeem.js
+++ pages/api/redeem.js
@@ -1,3 +1,19 @@
-export default function handler(req, res) {
-  res.status(200).json({ name: 'Acá va el response del redeem' })
-}
+import axios from "axios"
+
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+export default async (req, res) => {
+  const URL = 'https://coding-challenge-api.aerolab.co/redeem';
+  const data = { 
+    'productId': '5a0b35c1734d1d08bf7084c3',
+   };
+  const options = {
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': 'Bearer ' + process.env.SECRET_TECHZONE_API_KEY,
+    }
+  };
+  
+  const response = await axios.post(URL, data, options);
+  res.status(200).json({ data: response.data });
+};
